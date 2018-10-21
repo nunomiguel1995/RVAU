52152e30b8f32cb06bdb9df73becfe011d048f91
Nuno Miguel <up201406990@fe.up.pt>
2018-10-21 18:00:04 +0100

    Projectiles not moving with camera. Collisions added.

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -27,6 +27,5 @@ public class BulletScript : MonoBehaviour {
                 Destroy(temp_bullet, 5f);
             }
         }
-
 	}
 }
---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    public GameObject enemy;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("Enemy Collided");
+        Destroy(enemy);
+        Destroy(collisionInfo.gameObject);
+    }
+
+}
