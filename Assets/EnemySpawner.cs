1a93c2e15a134cbb7217cf51f9de090630cc4a35
Nuno Miguel <up201406990@fe.up.pt>
2018-11-05 12:06:03 +0000

    Enemy spawn and movement

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 1f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+
+    private int enemyCount = 0;
+    private float spawnTimer;
+    private List<GameObject> spawns = new List<GameObject>();
+
+    void Start(){
+        InvokeRepeating("Spawn", 1, 0.5f);
+    }
+
+    void Spawn()
+    {
+        if (enemyCount < 10)
+        {
+            if (Time.time > spawnTimer)
+            {
+                spawnTimer = Time.time + spawnTime;
+                int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+                GameObject spawn = Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+                spawns.Add(spawn);
+                enemyCount++;
+            }
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void Update()
+    {
+        foreach(GameObject g in spawns)
+        {
+            Debug.Log("Position " + transform.position);
+            g.transform.position += new Vector3(0.005f, 0, 0);
+            Debug.Log("New Position " + transform.position);
+        }
+    }
+}
